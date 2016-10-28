b8f0fca4a6112ce0e3d104984363901fe0c42e56
bajunkinizer <aldrinbuzz.manus@gmail.com>
2016-10-29 00:32:47 +0800

    Update Ball.java

--- Ball.java
+++ Ball.java
@@ -20,15 +20,15 @@ public class Ball {
 	int y = 250;
 	int xa = 0;
 	int ya = 1;
-        int score1 = 0;
-        int score2 = 0;
+        int score1 = 0; // added this in an effort to fix the scoring error in README, but it did not work
+        int score2 = 0;//
 	private Pong pong;
 
 	public Ball(Pong pong) {
 		this.pong = pong;
 	}
         
-        public Ball(Pong pong, int ya, int y, int score1, int score2) {
+        public Ball(Pong pong, int ya, int y, int score1, int score2) { //constructor for it to change its position depending on who last scored. And stores previous scores.
 		this.pong = pong;
                 this.ya = ya;
                 this.y = y;
@@ -48,19 +48,20 @@ public class Ball {
                 }
                 else if (y + ya < 0)
                 {
-                        score1++;
+                        score1++; // adds score if it gets past
 			pong.racquet1.setScore(score1);
-                        if( pong.racquet1.getScore() == 3)
+                        if( pong.racquet1.getScore() == 3) // if score gets to 3 then its game over
                         {
-                            pong.repaint();
+                            pong.repaint(); // repaint so that it display final score before ending the game
                             pong.gameOver();
+			    //resets score and everything just in case "gameOver()" doesn't end the game.
                             pong.racquet1.setScore(0);
                             pong.racquet2.setScore(0);
                             pong.racquet1.resetPos();
                             pong.racquet2.resetPos();
                             
                         }
-                        
+                        //resets ball and speed
                         pong.ball = new Ball(pong, -pong.speed, 450, score1, score2);
                         pong.speed = 1;
                         
@@ -68,6 +69,7 @@ public class Ball {
                 }
                 else if (y + ya > pong.getHeight() - DIAMETER)
                 {
+			//same logic above
                         score2++;
 			pong.racquet2.setScore(score2);
                         if( pong.racquet2.getScore() == 3)
@@ -83,17 +85,17 @@ public class Ball {
                          pong.ball = new Ball(pong, pong.speed, 30, score1, score2);
                          pong.speed = 1;
                 }
-                else if(collision1())
+                else if(collision1()) //racquet1
                 {
-                        xa = - pong.speed;
+                        xa = - pong.speed; // reverses direction after hitting
 			ya = -pong.speed;
 			y = pong.racquet1.getTopY() - DIAMETER;
-                        if(pong.speed < 5){
-                            pong.speed++;
+                        if(pong.speed < 5){ // I limited the max speed since its hard to play w/ higer speeds
+                            pong.speed++; // increases speed everytime ball collides
                         }
                      
 		}
-                else if(collision2()){
+                else if(collision2()){ //racquet2 same logic above
                     
                     xa = pong.speed;
                     ya = pong.speed;
@@ -104,8 +106,8 @@ public class Ball {
                 }
                 
            
-
-		x = x + xa;
+			//moves after x and y values have been finalized.
+		x = x + xa; 
 		y = y + ya;
 	}
 
